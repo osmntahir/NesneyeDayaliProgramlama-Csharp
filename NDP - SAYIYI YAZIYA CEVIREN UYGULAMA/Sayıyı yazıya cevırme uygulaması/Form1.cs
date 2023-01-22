77eac83115ddff5048683c2336a2c0a79af729de
osmantahir <fenerliosman14@gmail.com>
2023-01-22 12:24:10 +0300

    C#

---
+++ NDP - SAYIYI YAZIYA CEVIREN UYGULAMA/Sayıyı yazıya cevırme uygulaması/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+                            /****************************************************************************
+                            ** SAKARYA ÜNİVERSİTESİ
+                            ** BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+                            ** BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+                            ** NESNEYE DAYALI PROGRAMLAMA DERSİ
+                            ** 2021-2022 BAHAR DÖNEMİ
+                            **
+                            ** ÖDEV NUMARASI..........: 2
+                            ** ÖĞRENCİ ADI............: Osman Tahir Ozdemir
+                            ** ÖĞRENCİ NUMARASI.......: G211210059
+                            ** DERSİN ALINDIĞI GRUP...: B
+                            ****************************************************************************/
+
+namespace NDP_ODEV_2
+{
+    public partial class Form1 : Form
+        {
+          // FORM ELEMANLARINI OLUSTURMA
+                Label lblYazi = new Label();
+                TextBox txtSayi = new TextBox();
+                Label X = new Label();
+                Label Y = new Label();
+                Button btnHesapla = new Button();
+
+        public Form1()
+        {
+                 InitializeComponent();
+        }
+        // Form1 Uygulamasini Calistirdigimizda calisan metot
+        private void Form1_Load(object sender, EventArgs e)
+        {
+                // Formun ozelliklerini degistirme 
+                this.Width = 1000;
+                this.Height = 650;
+                this.Text = "Fonksiyon Hesabı";
+                // Textboximizin boyutunu belirleme
+                txtSayi.Width = 150;
+                txtSayi.Height = 25;
+                // Labelimizin boyutunu ve arkaplan rengini belirleme
+                lblYazi.Width = 700;
+                lblYazi.Height = 25;
+                lblYazi.BackColor = Color.LightGray;
+                // X labelinin boyutunu belirleme
+                X.Height = 25;
+                X.Width = 25;
+                X.Text = "X";
+                // Y labelinin boyutunu belirleme
+                Y.Height = 25;
+                Y.Width = 25;
+                Y.Text = "Y";
+                // Butonumuzun boyutunu belirleme
+                btnHesapla.Width = 90;
+                btnHesapla.Height = 50;
+                // Olusturdugumuz araclari forma ekleme
+                Controls.Add(txtSayi);
+                Controls.Add(X);
+                Controls.Add(Y);
+                Controls.Add(lblYazi);
+                Controls.Add(btnHesapla);
+                // X labelinin konumunu belirleme
+                X.Left = 80;
+                X.Top = 80;
+                // Y labelinin konumunu belirleme
+                Y.Left = 80;
+                Y.Top = 160;
+                // Textboximizin konumunu belirleme
+                txtSayi.Left = 150;
+                txtSayi.Top = 80;
+                // Yazi labelinin konumunu belirleme
+                lblYazi.Left = 150;
+                lblYazi.Top = 160;
+                // Butonumuzun konumunu belirleme
+                btnHesapla.Left = 165;
+                btnHesapla.Top = 250;
+                btnHesapla.Text = "Hesapla";
+                // Belirli fonksiyonlari olusturma
+                btnHesapla.Click += BtnHesapla_Click;
+                txtSayi.TextChanged += TxtSayi_TextChanged;
+        }
+        // Textboxa girilen degerleri kontrol eden metot
+        private void TxtSayi_TextChanged(object sender, EventArgs e)
+        {
+                        // Sadece rakam ve nokta gibi kullanilmasi gerekilen ozel sembollere izin veren if kosulu
+                 if (System.Text.RegularExpressions.Regex.IsMatch(txtSayi.Text, "[^0-9-.]"))
+                {
+                    MessageBox.Show("Lutfen sadece rakam veya nokta giriniz !");
+                    txtSayi.Text = "";
+                
+                }   
+        }
+        // Butona tiklandiginda islem yapan metot
+         private void BtnHesapla_Click(object sender, EventArgs e)
+        {
+                   // Gerekli string dizilerin olusturulmasi
+                     string[] birler = {"","Bir", "Iki", "Uc", "Dort", "Bes", "Alti", "Yedi", "Sekiz", "Dokuz" };
+                     string[] onlar = {"", "On", "Yirmi", "Otuz", "Kirk", "Elli", "Altmis", "Yetmis", "Seksen", "Doksan"};
+                     string[] yuzler = {"", "Yuz", "Iki Yuz", "Uc Yuz", "Dort Yuz", "Bes Yuz ", "Alti Yuz ", "Yedi Yuz", "Sekiz Yuz ", "Dokuz yuz " };
+                     string[] binler = {"", "Bin", "Iki Bin", "Uc Bin", "Dort Bin", "Bes Bin ", "Alti Bin ", "Yedi Bin", "Sekiz Bin ", "Dokuz Bin " };
+                    // Girilen TL degeri
+                   double Tl;
+                    Tl =  Convert.ToDouble(txtSayi.Text);
+                    // TL degerini basamaklara ayirma
+                    int BirlerTl = (int) Tl % 10;
+                    int OnlarTl = (int) (Tl%100) / 10;
+                    int Yuzler = (int) (Tl % 1000) / 100;
+                    int Binler = (int) (Tl % 10000) / 1000;
+                    int Onbinler = (int) (Tl % 100000) / 10000;
+                    // Kusurati olmayan Tl degiskeni. Ornegin 125.6 --> 125 e donusmesi
+                    double Tl2 = Math.Floor(Tl);
+                   // Kusuratli TL den Kusuratsiz TL yi cikarttigimizda kusurati buluruz ve bunu 100 ile carparak noktadan sayiyi elde ederiz
+                    double Kurus = (Tl-Tl2)*100;
+                    // Noktadan sonraki sayiyi basamaklara ayirma
+                    int OnlarKurus = (Convert.ToInt32(Kurus)) / 10;
+                    int BirlerKurus = (Convert.ToInt32(Kurus)) % 10;
+                    // En fazla 5 basamakli ve kusuratla en fazla 6 basamakli sayiyi kontrol eden if kosulu
+            if (Convert.ToDouble(txtSayi.Text) < 100000 && txtSayi.Text.Length <= 7)
+            {
+                    // Tum basamaklar 0 ise ekrana 0 tl yazdirilir
+                     if (Onbinler == 0 &&Binler ==0 && Yuzler == 0 && OnlarTl == 0 && BirlerTl == 0 && OnlarKurus == 0 && BirlerKurus == 0)
+                    {
+                        // lblYazi labelina yazdirma
+                        lblYazi.Text = "Sifir TL";
+                    }
+                    // Sadece kurus varsa sadece kurus olan kisim yazilir
+                    else if (Onbinler == 0 && Binler == 0 && Yuzler == 0 && OnlarTl == 0 && BirlerTl == 0)
+                    {
+                        // lblYazi labelina yazdirma
+                        lblYazi.Text = onlar[OnlarKurus] + " " + birler[BirlerKurus] + " Kurus";
+                    }
+                    // Kurus kismi yoksa sadece Tl kismi yazilir
+                    else if(OnlarKurus==0 && BirlerKurus==0) 
+                    {
+                        // lblYazi labelina yazdirma
+                        lblYazi.Text = onlar[Onbinler] + " " + binler[Binler] + " " + yuzler[Yuzler] + " " + onlar[OnlarTl] + " " + birler[BirlerTl] + " TL"; 
+                    }
+                    // Tum durumlarin yazildigi durum
+                    else
+                    {
+                        // lblYazi labelina yazdirma
+                        lblYazi.Text = onlar[Onbinler] + " " + binler[Binler] + " " + yuzler[Yuzler] + " " + onlar[OnlarTl] + " " + birler[BirlerTl] + " TL"
+                       + " " + onlar[OnlarKurus] + " " + birler[BirlerKurus] + " Kurus";
+                    }   
+                }
+            else
+            {
+                // Ekrana uyari mesaji verme
+                MessageBox.Show("Lutfen maksimum bes basamakli bir sayi giriniz ! ondalikli sayi ise 6 basamaga kadar girebilirsiniz ");
+                txtSayi.Text = "";
+            }
+        }
+        }
+    }
+
---
+++ NDP - SIFRE GUVENLIK UYGULAMASI/Sıfre guvenlık uygulaması/NDP-ODEV-1/Program.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace NDP_ODEV_1
+{
+    class SifreProgrami
+    {
+
+                                    /****************************************************************************
+                                    * SAKARYA ÜNİVERSİTESİ
+                                    ** BİLGİSAYAR VE BİLİŞİM BİLİMLERİ FAKÜLTESİ
+                                    ** BİLGİSAYAR MÜHENDİSLİĞİ BÖLÜMÜ
+                                    ** NESNEYE DAYALI PROGRAMLAMA DERSİ
+                                    ** 2021-2022 BAHAR DÖNEMİ
+                                    **
+                                    ** ÖDEV NUMARASI..........: 1
+                                    ** ÖĞRENCİ ADI............: Osman Tahir Özdemir
+                                    ** ÖĞRENCİ NUMARASI.......: G211210059
+                                    ** DERSİN ALINDIĞI GRUP...: 2-B
+                                    ****************************************************************************/
+
+        // BUYUK HARF SAYIISNI HESAPLAYAN METOT
+        static int BuyukHarfHesaplayici(string sifre)
+        {
+                        //Buyuk harf sayisini hesaplatma
+                        int BuyukHarfSayisi = sifre.Length - Regex.Replace(sifre, "[A-Z]", "").Length;
+                        // Buyuk harf sayisini geri dondurme
+                        return BuyukHarfSayisi;
+        }
+        // KUCUK HARF SAYISINI HESAPLAYAN METOT
+        static int KucukHarfHesaplayici(string sifre)
+        {
+                        // Kucuk harf sayisini hesaplatma
+                        int KucukHarfSayisi = sifre.Length - Regex.Replace(sifre, "[a-z]", "").Length;
+                        // Kucuk harf sayisini geri dondurme
+                        return KucukHarfSayisi;
+        }
+        // RAKAM SAYISINI HESAPLAYAN METOT
+        static int RakamHesaplayici(string sifre)
+        {
+                        // Rakam sayisini hesaplatma
+                        int RakamSayisi = sifre.Length - Regex.Replace(sifre, "[0-9]", "").Length;
+                        // Rakam sayisini geri dondurme
+                        return RakamSayisi;
+        }
+        // SEMBOL SAYISINI HESAPLAYAN METOT
+        static int SembolHesaplayici(string sifre)
+        {
+                        // Sembol Sayisini hesaplatma
+                        int SembolSayisi = sifre.Length - Regex.Replace(sifre, "[!-/:-@[-`{-~]", "").Length;
+                        // Sembol sayisini geri dondurme
+                        return SembolSayisi;
+        }
+        // Sifre Kurallarini Ekrana Yazdiran Metot
+        static void SifreOlusturmaKurallari()
+        {
+                        Console.WriteLine("***** SIFRE OLUSTURMA KURALLARI *****");
+                        Console.WriteLine();
+                        Console.WriteLine("En az 9 haneli bir sifre olsuturunuz");
+                        Console.WriteLine("Sifre olusturuken en az bir buyuk harf , kucuk harf , sembol ve rakam kullaniniz");
+                        Console.WriteLine();
+                        Console.WriteLine("***** SIFRE PUANLAMA KURALLARI *****");
+                        Console.WriteLine();
+                        Console.WriteLine("Her bir küçük harf 10 puan ve 20 puandan fazla küçük harften verilemez");
+                        Console.WriteLine("Her bir büyük harf 10 puan ve 20 puandan fazla büyük harften verilemez");
+                        Console.WriteLine("Her bir rakam 10 puan ve 20 puandan fazla rakamdan verilemez");
+                        Console.WriteLine("Her bir sembol 10 puandır.");
+                        Console.WriteLine();
+                        Console.WriteLine("***** SIFRE GUVENLIK KURALLARI *****");
+                        Console.WriteLine();
+                        Console.WriteLine("Genel puan 70 den küçük ise şifre kabul edilemez.");
+                        Console.WriteLine("Genel puan 70 ile 90 arasında ise şifre kabul edilir ");
+                        Console.WriteLine("Genel puan 90 ile 100 şifre kabul edilir ve güçlü.");
+                        Console.WriteLine();
+        }
+        // Main Metotu
+        static void Main(string[] args)
+        {
+                        SifreOlusturmaKurallari();
+                        // Sifre olusturma ve kullanicidan isteme
+                        string Sifre;
+                        Console.Write("Kurallara gore bir sifre giriniz : ");
+                        Sifre = Console.ReadLine();
+                        // Sifre puani degiskeni olusturma 
+                        int SifrePuani = 0;
+
+            // SIFRE OLUSMASI ICIN KARAR YAPILARI
+            if (Sifre.Length < 9) // Sifre.Length --> Sifrenin boyutunu verir
+            {
+                        Console.WriteLine("Sifre olsuturulamadi ! Lutfen en az 9 haneli bir sifre olusturun ");
+            }
+            else
+            {
+                    // Sifre olusturma kurallarindaki sartlari kontrol ettirme
+                      if (BuyukHarfHesaplayici(Sifre) == 0 || KucukHarfHesaplayici(Sifre) == 0 || RakamHesaplayici(Sifre) == 0 || SembolHesaplayici(Sifre) == 0)
+                    {
+                              Console.WriteLine("Sifre olusturulamadi ! Lutfen en az bir buyuk harf , kucuk harf , sembol veya rakam giriniz !!");
+                    }
+                    else
+                    {
+                            //SIFRE OLUSTURUKDU
+
+                            // OLUSTURULAN SIFREYI PUANLAMA KISMI
+
+                            // Eger 20 puan ve daha az bir puan almissa 10 ile carparak bulunur. Mesela 1 tane buyuk harf varsa 1*10 || 2 tane varsa 2*10
+                            // Aksi durumda ise yani 20 yi asmissa 20 puan alir. Cunku 20 puana ulasmistir vc fazlasi verilemez
+                            if (BuyukHarfHesaplayici(Sifre) * 10 <= 20)
+                            {
+                                SifrePuani += BuyukHarfHesaplayici(Sifre) * 10;
+                            }
+                            else
+                            {
+                                SifrePuani += 20;
+                            }
+                            if (KucukHarfHesaplayici(Sifre) * 10 <= 20)
+                            {
+                                SifrePuani += KucukHarfHesaplayici(Sifre) * 10;
+                            }
+                            else
+                            {
+                                SifrePuani += 20;
+                            }
+                            if (RakamHesaplayici(Sifre) * 10 <= 20)
+                            {
+                                SifrePuani += RakamHesaplayici(Sifre) * 10;
+                            }
+                            else
+                            {
+                                SifrePuani += 20;
+                            }
+                                // Sembol sayisinda kisitlama olmadigi icin direkt 10 ile carpilir
+                                SifrePuani += SembolHesaplayici(Sifre) * 10;
+
+                            // Sifre hanesi 9 ise 10 puan verilme durumu 
+                            if (Sifre.Length == 9)
+                            {
+                                SifrePuani += 10;
+                            }
+
+
+                            // HESAPLANAN PUANA GORE SIFRENIN GUVENLIK DURUMU
+
+                            if (SifrePuani < 70)
+                            {
+                                Console.WriteLine("Yetersiz guclukte sifre. Lutfen kurallara gore daha guclu bir sifre giriniz");
+                            }
+                            else if (SifrePuani >= 70 && SifrePuani < 90)
+                            {
+                                Console.WriteLine("Orta guclukte bir sifre olusturuldu");
+                            }
+                            else if (SifrePuani >= 90 && SifrePuani <= 100)
+                            {
+                                Console.WriteLine("Guclu bir sifre olusturuldu");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Tebrikler !! Coook Gucluu bir sifre olusturulduu !!");
+                            }
+
+                            Console.WriteLine("Sifre puaniniz : " + SifrePuani);
+                        }
+                    }
+             }
+    }
+}
+
+                                                                            // ODEV BITMISTIR :))
